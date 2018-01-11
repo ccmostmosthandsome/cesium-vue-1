57a30fb384a7ff41d7ae6f9cad32143c0f2c819f
solocao <solocao@126.com>
2018-01-11 14:25:19 +0800

    已经可以完整跑起来了，下一步就是优化

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,5 +1,10 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopywebpackPlugin = require('copy-webpack-plugin');
+const cesiumSource = '../node_modules/cesium/Source';
+const cesiumWorkers = '../Build/Cesium/Workers';
 
 module.exports = {
   /* 输入文件 */
@@ -11,7 +16,18 @@ module.exports = {
     filename: 'build.js',
     /* 静态目录，可以直接从这里取文件 */
     publicPath: '/dist',
+    sourcePrefix: ''
 
+  },
+  amd: {
+    // Enable webpack-friendly use of require in Cesium
+    toUrlUndefined: true
+  },
+  resolve: {
+    alias: {
+      // Cesium module name
+      cesium: path.resolve(__dirname, cesiumSource)
+    }
   },
   module: {
     rules: [
@@ -26,7 +42,30 @@ module.exports = {
         use: 'babel-loader',
         /* 排除模块安装目录的文件 */
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }, {
+        test: /\.(png|gif|jpg|jpeg|svg|xml|json)$/,
+        use: ['url-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new CopywebpackPlugin([{ from: 'node_modules/cesium/Build/Cesium/Workers', to: 'Workers' }]),
+    new CopywebpackPlugin([{ from: 'node_modules/cesium/Source/Assets', to: 'Assets' }]),
+    new CopywebpackPlugin([{ from: 'node_modules/cesium/Source/Widgets', to: 'Widgets' }]),
+    new webpack.DefinePlugin({
+      // Define relative base path in cesium for loading assets
+      CESIUM_BASE_URL: JSON.stringify('')
+    })
+  ],
+  node: {
+    // Resolve node module use of fs
+    fs: 'empty'
+  },
 }
--- src/app.vue
+++ src/app.vue
@@ -1,15 +1,31 @@
 <template>
-    <div id="app">
-        <h1>{{ msg }}</h1>
+  <div id="app">
+    <div id="cesiumContainer"> 
     </div>
+  </div>
 </template>
 
 <script>
+import Cesium from "cesium/Cesium";
+require("cesium/Widgets/widgets.css");
 export default {
   data() {
-    return {
-      msg: "Hello World!"
-    };
+    return {};
+  },
+  mounted() {
+    const viewer = new Cesium.Viewer("cesiumContainer");
   }
 };
 </script>
+
+<style>
+html,
+body,
+#cesiumContainer {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+}
+</style>
